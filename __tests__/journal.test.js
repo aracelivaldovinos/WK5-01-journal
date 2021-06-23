4a4e0e246146f65b02bd3ca1ba2cbb6ccdaf8385
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-23 16:47:42 -0500

    add method that count number words

--- __tests__/journal.test.js
+++ __tests__/journal.test.js
@@ -2,8 +2,13 @@ import { TestScheduler } from 'jest-cli';
 import Entry from './../src/journal.js';
 describe('Entry', () => {
   test('should create a entry object with two strings', () =>{
-    const entry = new Entry("Today","Today was great.")
+    const entry = new Entry("Today",["Today", "was", "great"])
     expect(entry.title).toEqual("Today")
-    expect(entry.body).toEqual("Today was great.")
+    expect(entry.body).toEqual(["Today", "was", "great"])
   })
+  test('should count the number words in body', () =>{
+    const entry = new Entry("Today",["Today" ,"was" ,"great"])
+    expect(entry.wordCount()).toEqual(3)
+  })
+ 
 })
--- src/journal.js
+++ src/journal.js
@@ -2,3 +2,8 @@ export default function Entry (title,body) {
   this.title = title
   this.body = body
 }
+
+Entry.prototype.wordCount = function (){
+  let wordCount = this.body.length
+  return wordCount
+}
