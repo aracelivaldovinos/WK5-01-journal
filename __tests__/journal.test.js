752ef027c3eb17457e10b5b26176de7c87240984
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-24 19:11:44 -0500

    complete businesslogic and test

--- __tests__/journal.test.js
+++ __tests__/journal.test.js
@@ -11,16 +11,44 @@ describe('Entry', () => {
     expect(entry.wordCount()).toEqual(3)
   })
   test('should recognize a vowel in a word', () =>{
-    const entry = new Entry("Today",["Tadd"])
-    expect(entry.vowelCount()).toEqual(1)
+    const entry = new Entry("Today","hello")
+    expect(entry.vowelCheck()).toEqual(true)
   })
-  test('should recognize any vowels in a word', () =>{
-    const entry = new Entry("Today",["Today"])
-    expect(entry.vowelCount()).toEqual(1)
+  test('should recognize that there is not a vowel in a word', () =>{
+    const entry = new Entry("Today","hll")
+    expect(entry.vowelCheck()).toEqual(false)
   })
-  test('should recognize any vowels in 2 or more words', () =>{
-    const entry = new Entry("Today",["Today","Eat"])
-    expect(entry.vowelCount()).toEqual(1)
+  test('should count when there is a vowel in a word', () =>{
+    const entry = new Entry("Today","hello")
+    expect(entry.vowelCount()).toEqual(2)
   })
+  test('should count when there is a vowel in more that one word', () =>{
+    const entry = new Entry("Today","hello its a beautiful day ")
+    expect(entry.vowelCount()).toEqual(10)
+  })
+  test('should recognize a consonant', () =>{
+    const entry = new Entry("Today","b")
+    expect(entry.consonantCheck()).toEqual(true)
+  })  
+  test('should count # of consonant in a word', () =>{
+    const entry = new Entry("Today","hello")
+    expect(entry.consonantCount ()).toEqual(3)
+  }) 
+  test('should split strings into sentences', () =>{
+    const entry = new Entry("Today","hello my name. is araceli!")
+    expect(entry.getTeaser1 ()).toEqual(["hello my name.", "is araceli!"])
+  }) 
+  test('should split first sentence', () =>{
+    const entry = new Entry("Today","hello my name. is araceli!")
+    expect(entry.getTeaser2 ()).toEqual(["hello", "my", "name."])
+  }) 
+  test('should display first 8 words in a sentence', () =>{
+    const entry = new Entry("Today","hello my name is araceli i like gum and candy. is araceli!")
+    expect(entry.getTeaser3 ()).toEqual("hello my name is araceli i like gum")
+  }) 
+  test('should display first 8 words in a sentence', () =>{
+    const entry = new Entry("Today","hello my name is araceli. is araceli!")
+    expect(entry.getTeaser3 ()).toEqual("hello my name is araceli.")
+  }) 
  
 })
--- src/journal.js
+++ src/journal.js
@@ -8,13 +8,93 @@ Entry.prototype.wordCount = function (){
   return wordCount
 }
 
-Entry.prototype.vowelCount = function () {
+
+Entry.prototype.vowelCheck = function () {
   const vowels = ["a","e","i","o","u"]
-  let vowelCount = 0
-  for (let i=0; i<=vowels.length;i++){
-    if (this.body[i].includes(vowels[i]))  {
-      return 1
+  let string = this.body.split(" ")
+  let count = 0
+  for (let i=0; i<vowels.length;i++){
+    for (let j=0; j < string.length; j++ ) {
+      if (string[j].includes(vowels[i]))  {
+       return true        
+    }
+    
+    
     }
   } 
-  
+  return false
 }
+
+Entry.prototype.vowelCount = function () {
+  const vowels = ["a","e","i","o","u"]
+  let string = this.body.split("")
+  let count = 0
+  for (let i=0; i<vowels.length;i++){
+    for (let j=0; j < string.length; j++ ) {
+      if (string[j].includes(vowels[i]))  {
+        count++
+        
+    }
+    
+    
+    }
+  } 
+  return count
+}
+
+Entry.prototype.consonantCheck = function () {
+  const consonats = ["b","c","d","f","g","h","i","j","k","l","m","n","","p","q","r","s","t","v","w","z"]
+  let string = this.body.split(" ")
+  let count = 0
+  for (let i=0; i<consonats.length;i++){
+    for (let j=0; j < string.length; j++ ) {
+      if (string[j].includes(consonats[i]))  {
+       return true        
+    }
+    
+    
+    }
+  } 
+  return false
+}
+
+Entry.prototype.consonantCount = function () {
+  const consonants = ["b","c","d","f","g","h","i","j","k","l","m","n","p","q","r","s","t","v","w","z"]
+  let string = this.body.split("")
+  let count = 0
+  for (let i=0; i<consonants.length;i++){
+    for (let j=0; j < string.length; j++ ) {
+      if (string[j].includes(consonants[i]))  {
+        count++
+        
+    }
+    
+    
+    }
+  } 
+  return count
+}
+
+Entry.prototype.getTeaser1 = function () {
+  let sentence = this.body.replace(/(\.+|\:|\!|\?)(\"*|\'*|\)*|}*|]*)(\s|\n|\r|\r\n)/gm, "$1$2|").split("|")
+  return sentence
+}
+
+Entry.prototype.getTeaser2 = function () {
+  let sentence = this.body.replace(/(\.+|\:|\!|\?)(\"*|\'*|\)*|}*|]*)(\s|\n|\r|\r\n)/gm, "$1$2|").split("|")
+ let words = sentence[0].split(" ")
+ return words
+}
+
+Entry.prototype.getTeaser3 = function () {
+let sentence = this.body.replace(/(\.+|\:|\!|\?)(\"*|\'*|\)*|}*|]*)(\s|\n|\r|\r\n)/gm, "$1$2|").split("|")
+let words = sentence[0].split(" ")
+
+ if (words.length >= 8) {
+   let newSentence = (words[0]+ " "+words[1]+ " "+words[2]+ " "+words[3] +" "+words[4]+" "+ words[5] +" "+ words[6] +" "+ words[7])
+   return newSentence
+ }else {
+   return sentence[0]
+ }
+}
+ 
